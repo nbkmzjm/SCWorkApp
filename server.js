85085be8bf8b573f328235b53105ab4c40574cf6
nbkmzjm <ngokhanhthien@yahoo.com>
2017-01-09 19:27:25 -0700

    1-9 setup db to keep data last 90 days

--- server.js
+++ server.js
@@ -536,6 +536,41 @@ umzug.up().then(function (migrations) {
 		
 		http.listen(PORT, function() {
 			console.log('Helllo Express server started on PORT ' + PORT);
+			setInterval(function(){
+		        console.log('clean DB has began')
+		        var prior90Date = moment(new Date()).subtract(90,'days').format()
+		        
+		          console.log('prior90Date:'+prior90Date)
+		        db.assignTracer.destroy({
+		          where:{
+		            updatedAt:{
+		              $lt:prior90Date
+		            }
+		          }
+		        }).then(function(){
+
+
+			        return [db.assign.destroy({
+						where:{
+							updatedAt:{
+							  $lt:prior90Date
+							}
+						}
+			        })]
+		        }).then(function(){
+					var prior10Date = moment(new Date()).subtract(10,'days').format()
+					return [db.token.destroy({
+						where:{
+							createdAt:{
+								$lt:prior10Date
+							}
+						}
+					})]
+
+		        }).catch(function(e) {
+		          console.log(e);
+		        });
+      		}, 30000);
 		});
 
 	});
