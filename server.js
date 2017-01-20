a0d9d7e8da97e48f9083030aca7b86558a758366
nbkmzjm <ngokhanhthien@yahoo.com>
2017-01-20 16:33:53 -0700

    1-20 add Schedule overview

--- server.js
+++ server.js
@@ -225,6 +225,53 @@ app.post('/taskSC', middleware.requireAuthentication, function(req, res){
 	})
 })
 
+app.post('/scOverview', middleware.requireAuthentication, function(req, res){
+	var curUser= req.user;
+	var sDate = req.body.sDate
+	var eDate = req.body.eDate
+	var userId = req.body.userId
+	var datePosRange = [];
+	console.log(userId)
+
+	var datePos={
+		$between:[sDate,eDate]
+	}
+
+	console.log(sDate +':'+ eDate)
+	if (sDate.slice(-1)!=eDate.slice(-1)){
+		for(var i = 0; i<49;i++){
+			datePosRange.push(moment(new Date(sDate)).add(i,'days').format('MM-DD-YYYY'))
+		}
+		datePos = {$in:datePosRange}
+	}
+	
+	db.assign.findAll({
+		attributes:['id', 'datePos', 'Memo', 'userId', 'Note'],
+		include:[{
+			model:db.assignTracer,
+			include:[{
+				model:db.user
+			}]
+			
+		}],
+		where:{
+			datePos,
+			userId:userId
+
+		},
+		order:[
+				[db.assignTracer,'createdAt', 'DESC']
+			]
+	}).then(function(assign){
+		
+		// console.log(JSON.stringify(assign, null, 4))
+		res.json({
+			assign,
+			 curUser
+		})
+	})
+})
+
 
 app.post('/assignTracerReadUpd', middleware.requireAuthentication, function(req, res) {
 	var assignTracerId = req.body.assignTracerId;
